1fdbaab4a97e74c95be1f4e6c3b5d8a86949ba22
dogodogodoggo <do@g.go>
2020-06-21 03:55:46 +0900

    create v2

--- dockerfile
+++ dockerfile
@@ -2,3 +2,4 @@ FROM python:3.7.4
 ARG project_directory
 WORKDIR $project_directory
 RUN pip install flask
+RUN pip install PyJWT
--- src/app.py
+++ src/app.py
@@ -1,18 +1,22 @@
 from flask import Flask, jsonify, request, Response
 import base64
+import jwt
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
+with open(".secrets") as f:
+    SECRETS = f.read().rstrip()
+
 @app.route('/')
 def index():
-  return jsonify({
-    "message": "Hi!! this is my first API. project is available at : https://github.com/dogodogodoggo/doggo_api.git"
-  })
+    return jsonify({
+        "message": "Hi!! this is my first API. project is available at : https://github.com/dogodogodoggo/doggo_api.git"
+    })
 
 
 @app.route('/api/v1/hello/<string:name>', methods=['GET'])
-def create_user(name):
+def create_user_v1(name):
     if name == "admin":
         return jsonify({
             "message": "Bad Request, woof...."
@@ -39,5 +43,34 @@ def doggo_v1():
     return jsonify(resp)
 
 
+@app.route('/api/v2/hello/<string:name>', methods=['GET'])
+def create_user_v2(name):
+    if name == "admin":
+        return jsonify({
+            "message": "Bad Request, woof...."
+        }), 400
+    encoded = jwt.encode({'name': name}, SECRETS, algorithm='HS256')
+    return jsonify({
+        "token": encoded.decode('utf-8')
+    })
+
+
+@app.route('/api/v2/doggo', methods=['POST'])
+def doggo_v2():
+    token = request.json.get('token')
+    decoded = jwt.decode(token, SECRETS, algorithms='HS256')
+    name = decoded["name"]
+
+    if name == "admin":
+        resp = {
+            "flag" : "m1z0r3{xxxxxxxxx}"
+        }
+    else:
+        resp = {
+            "doggo" : f"bowwow, {name}"
+        }
+    return jsonify(resp)
+
+
 if __name__ == '__main__':
-  app.run()
+    app.run()
